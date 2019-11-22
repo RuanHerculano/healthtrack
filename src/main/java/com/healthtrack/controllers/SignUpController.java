c603c305fd8f334c2751adaaab49a93827339b41
Ruan Herculano <ruannawe95@gmail.com>
2019-11-22 01:12:30 -0300

    adiciona notação do spring para controller

--- src/main/java/com/healthtrack/controllers/SignUpController.java
+++ src/main/java/com/healthtrack/controllers/SignUpController.java
@@ -1,10 +1,11 @@
 package com.healthtrack.controllers;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+@Controller
 public class SignUpController {
 
     @GetMapping("/signup")
